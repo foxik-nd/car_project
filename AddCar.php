58b6137c7393252dcf8f485a9aba650a36769baf
Reelecks <robinlandais75@yahoo.com>
2022-10-04 12:18:55 +0200

    comm add

--- AddCar.php
+++ AddCar.php
@@ -3,13 +3,15 @@
 <head>
     <title>Concession IPSSI</title>
     <meta charset="UTF-8">
+    <!-- insertion du css et bootstrap -->
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <header>
+<!-- navbar -->
 <nav class="navbar sticky-top navbar-expand-lg" style="background-color: #150cd2">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="Index.php">
     <img src="logoI.png" width="30" height="30" class="d-inline-block align-top" alt="">Concession IPSSI
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,6 +25,7 @@
   </div>
   </nav>
 </header>
+<!-- Container mot de passe et bouton valider -->
 <div class="containerMDP">
 <h4>Entrer le mot de passe</h4>
         <form method="post">
@@ -31,8 +34,8 @@
         </form>
 </div>
 <?php
-$resultat=0;
-include 'Functions.php';
+//vérification si MDP est correct. Si oui redirection au formulaire pour ajouter un véhicule
 if(isset($_POST['Valider']) && ($_POST['MDP']=="IPSSI")) {
     header("Location: https://airtable.com/shrXG9QO1vjvW2KPH");
 }
+?>
