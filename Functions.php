f6e104ae976b1fb4d9bb43c0eace5a68a856cdf7
Reelecks <robinlandais75@yahoo.com>
2022-10-06 10:07:43 +0200

    aa

--- Functions.php
+++ Functions.php
@@ -179,8 +179,11 @@ function getOptionMoteur()
     }
 
 }
-
 ?>
+
+
+
+
 <!-- lien bootsrap -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
